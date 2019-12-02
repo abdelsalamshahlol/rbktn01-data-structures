3d1105480518614fff7ba5855352c09469c23665
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-02 15:29:10 +0100

    stack progress

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.key = 0;
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.key++] = value;
+};
 
+Stack.prototype.pop = function() {
+  if (this.key <= 0) {
+    return;
+  }
+  --this.key;
+  var deleted = this.storage[this.key];
+  delete this.storage[this.key];
+  return deleted;
+};
+
+Stack.prototype.size = function() {
+  return this.key;	
+};
