8682071a9b27072cd9fdb91b9bd223981a70b7c3
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-02 14:25:36 +0100

    stack progress

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+
+  },
+
+  pop: function() {
+
+  },
+
+  size: function() {
+
+  }
+};
 
 
