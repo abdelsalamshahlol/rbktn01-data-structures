793f7e84d1e0d1960dfcea7994464080b250c308
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-02 14:02:02 +0100

    stack progress

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
+
+  someInstance.storage = storage;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[key++] = value;
   };
 
   someInstance.pop = function() {
+    // get the last value 
+    var deletes = storage[key - 1]; 
+    // decrease the size of storage
+    delete storage[key - 1];
+    key--;
+    // return the last value
+    return deletes;
   };
 
   someInstance.size = function() {
+    return key;
   };
 
   return someInstance;
