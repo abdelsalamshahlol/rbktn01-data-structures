40738756288c7dc2e4d29294f015999732e22ba1
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-02 15:02:21 +0100

    stack progress

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,6 +2,8 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
+  someInstance.key = 0;
+  someInstance.storage = {};
   _.extend(someInstance, stackMethods);
 
   return someInstance;
@@ -9,11 +11,17 @@ var Stack = function() {
 
 var stackMethods = {
   push: function(value) {
-
+    this.storage[this.key++] = value;
   },
 
   pop: function() {
-
+    if (this.key < 0) {
+      return;
+    }
+    --this.key;
+    var delets = this.storage[this.key];
+    delete this.storage[this.key];
+    return delets;
   },
 
   size: function() {
