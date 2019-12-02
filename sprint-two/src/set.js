40dbb848ff47055d08abccef5251a75765804a01
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-02 20:00:09 +0100

    set progress

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,26 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  // ternary operator
+  return this._storage[item] ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    var temp = this._storage[item];
+    delete this._storage[item];
+    return temp;
+  }
 };
 
 /*
