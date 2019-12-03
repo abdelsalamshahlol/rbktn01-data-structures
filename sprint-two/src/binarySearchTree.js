37d339a57d1df27e320ed16d3bd94153093cc43a
Abdelsalam Shahlol <abdulsalamshahlol@gmail.com>
2019-12-03 15:15:08 +0100

    BST progress

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,38 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function(value) {
+  // First create a node
+  let node = arguments[1] || new BinarySearchTree(value);
+  let x = arguments[1];
+
+  if (x === undefined) {
+    console.log(this, node, x);
+    return;
+  }
+
+  // If new node value < parent node value
+  if ( value < this.value ) {
+    //	Place on left
+    this.left = node;
+    // If new node value > parent node value
+  } else if ( value > this.value ) {
+    // 	Place on right
+    this.right = node;
+  }
+  this.insert(value, this);
+  // return node;
+};
+
+BinarySearchTree.prototype.contains = function(value) {
+  // body...
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(value) {
+  // body...
 };
 
 
